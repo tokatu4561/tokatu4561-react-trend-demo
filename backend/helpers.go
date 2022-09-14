a375a76e4a1b794ad362929870dd0c22c8c76b58
tokatu4561 <ryobsk4561@gmail.com>
2022-09-14 23:11:51 +0900

    helper function readJson what read json request

---
+++ backend/helpers.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func(app *application) readJson(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1048576
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
