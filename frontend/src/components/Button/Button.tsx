c7d21a371051df1495b399e0f0e5794fa1a1de56
tokatu4561 <ryobsk4561@gmail.com>
2022-09-15 23:38:48 +0900

    add button component

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ type = 'button', children }, ref) => {
+    return (
+      <button
+        ref={ref}
+        type={type}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        {children}
+      </button>
+    )
+  }
+)
