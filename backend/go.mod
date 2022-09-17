7958b0929234f1554d5e7577f893a18439e77aed
tokatu4561 <ryobsk4561@gmail.com>
2022-09-17 20:24:05 +0900

    jwt ライブラリ最新に jwt認証api追加

--- backend/go.mod
+++ backend/go.mod
@@ -3,9 +3,9 @@ module myapp
 go 1.18
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/stripe/stripe-go/v73 v73.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
--- backend/handler.go
+++ backend/handler.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"myapp/internal/cards"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 type stripePayload struct {
@@ -89,6 +95,7 @@ func (app *application) CreateAuthnicateToken(w http.ResponseWriter, r *http.Req
 	user, err := app.DB.GetByEmail(userInput.email)
 	if err != nil {
 		app.badRequest(w, r, err)
+		return
 	}
 
 	// パスワード検証
@@ -98,5 +105,78 @@ func (app *application) CreateAuthnicateToken(w http.ResponseWriter, r *http.Req
 		return
 	}
 
-	// token生成
+	if (isValidPassword) {
+		app.badRequest(w, r, errors.New("パスワードが一致しません"))
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"user_id": 12345678,
+			"exp": time.Now().Add(time.Hour * 24).Unix(),
+			})
+	
+	// TODO:: エラーログ残す　後でやる
+	tokenString, _ := token.SignedString([]byte("SECRET_KEY"))
+
+	var response struct {
+		Error bool `json:"error"`
+		Token string `json:"token"`
+	}
+
+	response.Error = false
+	response.Token = tokenString
+
+	app.writeJson(w, 200, response)
+}
+
+func (app *application) GetAuthUser(w http.ResponseWriter, r *http.Request) {
+	payloadToken, err := app.getAuthenticateTokenFromHeader(r)
+	if err != nil {
+		app.badRequest(w, r, err)
+	}
+
+	//　tokenを検証
+	token, _ := jwt.Parse(payloadToken, func(token *jwt.Token) (interface{}, error) {
+		// tokenアルゴリズムを検証
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte("SECRET_KEY"), nil
+	})
+
+	claims, ok := token.Claims.(jwt.MapClaims); 
+	if ok && token.Valid {
+		var response struct {
+			Error bool `json:"error"`
+			UserId float64 `json:"user_id"`
+		}
+	
+		response.Error = false
+		response.UserId = claims["user_id"].(float64)
+	
+		app.writeJson(w, 200, response)
+	} else {
+		app.badRequest(w, r, errors.New("認証エラー"))
+	}
+}
+
+// ヘッダーのbearerからトークンを取り出す
+func (app *application) getAuthenticateTokenFromHeader(r *http.Request) (string, error) {
+	authorizationHeader := r.Header.Get("Authorization")
+	if authorizationHeader == "" {
+		return "", errors.New("no authorization header received")
+	}
+
+	headerParts := strings.Split(authorizationHeader, " ")
+	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+		return "", errors.New("no authorization header received")
+	}
+
+	token := headerParts[1]
+	if len(token) != 26 {
+		return "", errors.New("authentication token wrong size")
+	}
+
+	return token, nil
 }
