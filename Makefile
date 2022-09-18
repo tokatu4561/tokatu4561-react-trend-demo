3425f8931f68bab78461ac24a7e54ed361bc2690
tokatu4561 <ryobsk4561@gmail.com>
2022-09-18 10:01:14 +0900

    mysqlでmigrationが上手くいかないのでpostgressに変更

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
 STRIPE_SECRET=
 STRIPE_KEY=
-DATABASE_URL=
+DATABASE_URL='postgres://postgres:password@localhost:5432/practice?sslmode=disable'
+# DSN="host=postgres port=5432 user=postgres password=password dbname=users sslmode=disable timezone=UTC connect_timeout=5"
+
+up:
+	@echo "build docker conteiner"
+	@docker compose up -d --build
 
 ## migrate: migration db data
 migrate:
 	@echo "migration db data..."
-	@migrate -database ${DATABASE_URL} -path database/migrations up
+	@migrate -database ${DATABASE_URL} -path backend/database/migrations/ up
 
+down-all:
+	@docker-compose down --rmi all --volumes --remove-orphans
--- backend/database/migrations/20220913231747_create_users_table.down.sql
+++ backend/database/migrations/20220913231747_create_users_table.down.sql
@@ -1,9 +1,15 @@
-CREATE TABLE IF NOT EXISTS users
-(
-    id serial,
-    name varchar(50) NOT NULL,
-    email varchar(256) NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS users(
+    user_id serial PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (50) NOT NULL,
+    email VARCHAR (300) UNIQUE NOT NULL
 );
+
+
+-- mysql
+-- CREATE TABLE IF NOT EXISTS users(
+--     user_id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR (50) NOT NULL,
+--     password VARCHAR (50) NOT NULL,
+--     email VARCHAR (300) NOT NULL
+-- );
