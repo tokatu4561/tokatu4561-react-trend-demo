956d00c50f983e4d81351637da762be226259e6e
tokatu4561 <ryobsk4561@gmail.com>
2022-09-18 12:02:29 +0900

    migrationファイル up と downの内容が逆だった

--- backend/database/migrations/20220913231747_create_users_table.down.sql
+++ backend/database/migrations/20220913231747_create_users_table.down.sql
@@ -1,15 +1,1 @@
-CREATE TABLE IF NOT EXISTS users(
-    user_id serial PRIMARY KEY,
-    username VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (50) NOT NULL,
-    email VARCHAR (300) UNIQUE NOT NULL
-);
-
-
--- mysql
--- CREATE TABLE IF NOT EXISTS users(
---     user_id NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR (50) NOT NULL,
---     password VARCHAR (50) NOT NULL,
---     email VARCHAR (300) NOT NULL
--- );
+DROP TABLE IF EXISTS users;
